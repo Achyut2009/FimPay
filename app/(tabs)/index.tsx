c379fcf453e9af1602c5d10f6970da208ce74498
Achyut Paliwal <achyutkpaliwal@gmail.com>
2026-02-09 21:12:08 +0300

    Co-authored-by: mywayarclinux <mywayarclinux@gmail.com>

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home(){
   return(
     <View className="flex min-h-screen">
-      <View className="">hello, hi, and welcome toooo FimPay!!!</View>
+      <View className="">hello, hi, and welcome toooo FimPay!!</View>
     </View>
   );
 }
